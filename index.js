8f34931cdd3e52fa2dd827d31428d7931906f98e
Ebrima Sise <siseebrima@gmail.com>
2021-09-26 14:24:36 +0000

    uibuild

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 // const Note = requiodels/note");
 // const Note = require("./models/note");
-import Note from "./models/note.js";
 import dotenv from "dotenv";
+import Note from "./models/note.js";
 
 dotenv.config();
 
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("build"));
 
-Note.find({}).then((result) => {
-  console.log(result);
-});
+// Note.find({}).then((result) => {
+//   console.log(result);
+// });
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello Ebrima</h1>");
@@ -29,19 +29,9 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-
-  //   if (note) {
-  //     response.json(note);
-  //   } else {
-  //     response.status(404).end();
-  //   }
-  note
-    ? response.json(note)
-    : response
-        .status(404)
-        .send("opps! The resource you are looking for does not exist");
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
 });
 
 app.delete("/api/notes/:id", (req, res) => {
@@ -58,26 +48,20 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
   });
-  // notes = notes.concat(note);
-  // response.json(note);
-  note.save().then((result) => {
-    console.log(`the note is successfully saved`);
-    mongoose.connection.close();
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
   });
 });
-
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Ebrima, the server is listening on port ${PORT}`);
